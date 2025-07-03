ff0673c23a9f077fc1511a619d20777c0527ad98
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2025-07-03 17:05:34 +0530

    add : rate limit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const logRoutes = require('./routes/logs');
-
+const rateLimit = require('express-rate-limit');
 const app = express();
+
 const PORT = 3000;
+
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30, // 30 reqs
+  message: "Too many requests, please try again later.",
+});
+
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
